30751b9e145a88ade348c729ad88c59fad963bfd
Kaue Ribeiro Costa <ribeirokaue70@gmail.com>
2025-10-07 08:09:07 -0400

    feat: aditing ec2 instances

--- README.md
+++ README.md
@@ -1,12 +1,20 @@
+# Estudo realizado
+
+## Objetivo 
+O objetivo desse repositório é realizar um estudo em cima de um ptrojet oficiar do terraform que utiliza o Meta-argumetns Count para construir um infraestrutura completa na AWS. O foco aqui vai ser como o Terraform Count trabalha. 
+
 
 # Meta-arguments Count Terraform
 O meta-arguments count é um recurso na Terraform para vc gerenciar multiplos blocos de recursos ou modulos com um numeri especifico de incrementação. O ideal é usar ele quando tem varios recursos quase iguais.
+
 
 
 ## Recursos a serem criados na AWS
 * VPC
 * Load Balancer
 * Instâncias EC2
+
+Onde esta o Count? 
 
 
 
@@ -17,3 +25,4 @@ O meta-arguments count é um recurso na Terraform para vc gerenciar multiplos bl
 # **Referências**
 https://developer.hashicorp.com/terraform/language/meta-arguments#count
 https://developer.hashicorp.com/terraform/tutorials/0-13/count
+https://github.com/hashicorp-education/learn-terraform-count
--- variables.tf
+++ variables.tf
@@ -7,7 +7,7 @@ variable "aws_region" {
 variable "project_name" {
   description = "Name of the project. Used in resource names and tags."
   type        = string
-  default     = "client-webapp"
+  default     = "demo-webapp"
 }
 
 variable "environment" {
@@ -28,27 +28,10 @@ variable "private_subnets_per_vpc" {
   default     = 2
 }
 
-variable "private_subnet_cidr_blocks" {
-  description = "Available cidr blocks for private subnets."
-  type        = list(string)
-  default = [
-    "10.0.101.0/24",
-    "10.0.102.0/24",
-    "10.0.103.0/24",
-    "10.0.104.0/24",
-    "10.0.105.0/24",
-    "10.0.106.0/24",
-    "10.0.107.0/24",
-    "10.0.108.0/24",
-    "10.0.109.0/24",
-    "10.0.110.0/24",
-    "10.0.111.0/24",
-    "10.0.112.0/24",
-    "10.0.113.0/24",
-    "10.0.114.0/24",
-    "10.0.115.0/24",
-    "10.0.116.0/24"
-  ]
+variable "instance_type" {
+  description = "Type of EC2 instance to use."
+  type        = string
+  default     = "t2.micro"
 }
 
 variable "vpc_cidr_block" {
@@ -77,5 +60,28 @@ variable "public_subnet_cidr_blocks" {
     "10.0.14.0/24",
     "10.0.15.0/24",
     "10.0.16.0/24"
+  ]
+}
+
+variable "private_subnet_cidr_blocks" {
+  description = "Available cidr blocks for private subnets."
+  type        = list(string)
+  default = [
+    "10.0.101.0/24",
+    "10.0.102.0/24",
+    "10.0.103.0/24",
+    "10.0.104.0/24",
+    "10.0.105.0/24",
+    "10.0.106.0/24",
+    "10.0.107.0/24",
+    "10.0.108.0/24",
+    "10.0.109.0/24",
+    "10.0.110.0/24",
+    "10.0.111.0/24",
+    "10.0.112.0/24",
+    "10.0.113.0/24",
+    "10.0.114.0/24",
+    "10.0.115.0/24",
+    "10.0.116.0/24"
   ]
 }
